53a857930f8b4cd03f4f84bb64697564075e18b4
Victor Okech <vickesnet@gmail.com>
2022-06-23 13:41:27 +0300

    A few changes

--- app/Http/Livewire/ImageUpload.php
+++ app/Http/Livewire/ImageUpload.php
@@ -14,11 +14,12 @@
 		public $optimizedImage = '';
 		
 		public function mount() {
-			$this->media = 'https://res.cloudinary.com/dgrpkngjn/image/upload/v1655820836/content-aware/hd4yrwgvyi8mubj3t0mm.jpg';
-			$this->optimizedImage = 'https://res.cloudinary.com/dgrpkngjn/image/upload/w_700,h_800,c_scale/v1655820836/content-aware/hd4yrwgvyi8mubj3t0mm.jpg';
+			$this->media = 'https://res.cloudinary.com/dgrpkngjn/image/upload/v1655979457/cloudinary-speed/ypmrd0zhbyj0emfhthdm.jpg';
+			$this->optimizedImage = 'https://res.cloudinary.com/dgrpkngjn/image/upload/w_700,h_800,c_scale/v1655979445/cloudinary-speed/qcmetreekzyjc1dk2n8t.jpg';
 		}
 		
 		public function upload() {
+			/*First we validate the input from the user*/
 			$data = $this->validate([
 				'media' => [
 					'required',
@@ -31,6 +32,8 @@
 			if (empty($data['media'])) {
 				unset($data['media']);
 			} else {
+				/*We will now set the transformations required to optimize the images
+				 based on recommended optimization solutions*/
 				$cloud_name = env('CLOUDINARY_CLOUD_NAME', 'dgrpkngjn');
 				$folder = 'cloudinary-speed';
 				$media = $data['media'];
@@ -43,25 +46,28 @@
 				$optimal = cloudinary()->upload($media->getRealPath(), [
 					'folder'         => $folder,
 					'transformation' => [
-						'width'   => $width,
-						'height'  => $height,
-						'quality' => $quality,
-						'fetch_format'   => $fetch,
-						'crop'    => $crop
+						'width'        => $width,
+						'height'       => $height,
+						'quality'      => $quality,
+						'fetch_format' => $fetch,
+						'crop'         => $crop
 					]
 				])->getSecurePath();
 				
 				$non_optimal = cloudinary()->upload($media->getRealPath(), [
-					'folder'         => $folder
+					'folder' => $folder
 				])->getSecurePath();
 				
 				// Optimized image fetching
-				// Fetching an optimized image applying the transformations we specified
-				// which will compress, resize and optimize our images for a speedy web application
+				/* Fetching an optimized image applying the transformations we
+				specified which will compress, resize and optimize our images
+				for a speedy web application */
 				$slice = Str::afterLast($optimal, '/');
 				$optimized = "https://res.cloudinary.com/{$cloud_name}/image/upload/w_{$width},h_{$height},c_{$crop}/{$folder}/{$slice}";
 				
- 				$this->optimizedImage = $optimized;
+				$this->optimizedImage = $optimized;
+				
+				// Non optimized version for comparison
 				$this->media = $non_optimal;
 			}
 			
